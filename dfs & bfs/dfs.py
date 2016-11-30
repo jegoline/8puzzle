09d08c596bf466f7422524a4cb349cc5655559f5
Nicole Xirakia <nxirakia@gmail.com>
2016-11-30 01:03:35 +0100

    Bfs and Dfs improvements

--- dfs & bfs/dfs.py
+++ dfs & bfs/dfs.py
@@ -126,9 +126,44 @@ def setMove(move, row, col,board):          #Setting the moves based on the list
 def create_node(board, parent,sibling, moves, depth,visited):
     return Node(board, parent, sibling, moves, depth,visited)
 
-def cur_state(board):
-    moves = legal_moves(board)
-    return moves
+def cur_state(node,visited,goal,start):
+    goalReached = False
+
+    if node.parent==None and start==True:
+        state = node.board.ravel()  # make temp flat
+        state = int(''.join(map(str, state)))
+        visited.add(state)
+        start=False
+
+    elif len(node.moves) != 0:
+        temp = np.array(node.board)
+        br, bc = getBlankPosition(temp)
+        temp1 = setMove(node.moves[0], br, bc, temp)  # making my move
+      #  print(node.moves[0])
+        node.moves.pop(0)
+        state = temp1.ravel()  # make temp flat
+        state = int(''.join(map(str, state)))
+        v=False
+
+
+
+    if state not in visited:
+
+         visited.add(state)
+         # node.visited = True
+         new_moves= legal_moves(temp1)
+         d = node.depth + 1
+         print ("appending ", temp1)
+         children = node.add_child(create_node(temp1, node, None, new_moves, d, False))
+    else:
+        v=True
+
+    if np.all(state == goal):  # Checking is the current board is winning
+        print('I won!!')
+        goalReached = True
+
+    return start, goalReached, v
+
 
 def bfs(node, goal, depth):
     depth_limit = depth
@@ -139,75 +174,38 @@ def bfs(node, goal, depth):
     OFM=False       #Out Of Moves Boolean
     start=True
     d=0
+    goalStateB=False
 
-    while goalReached==False or OFM:
-
-        if len(nodes)==0 and start==True:
-           start=False
-           nodes.append(node)
-
-        if node.depth <= depth_limit:
-
-            if len(node.moves)==0 and node.has_children():
-
-                if (node.parent==None and len(node.children)!=0) or (node.parent==None and len(node.parent.children)==0):           #for the root
-                    node = node.children.pop(0)
-                    nodes.append(node)
-                elif node.parent!=None and len(node.parent.children)==0:
-                    nodes.pop(0)
-                    node=nodes[0]
-                    node= node.children[0]
-                elif node.parent!=None and (node.parent.has_children() and len(node.parent.moves) == 0):
-                    node = node.parent.children.pop(0)  # take the next sibling
-                    nodes.append(node)
-            elif len(nodes)!=0:
-                if node.moves==0 and node.children==0:
-                    node=nodes.pop(0)
-            elif len(nodes)==1:
-                if node.moves==0 and node.children==0:
-                    print "out of moves and i am ending the game here"
-                    OFM=True
-                break
-        elif node.depth > depth_limit:
-            print "I've reached the maximum depth"
-            break
+    while goalReached==False:
 
 
+        if len(nodes)==0 and start==True:       #Appending the root node
 
-        if np.all(node.board == goal):                  #Checking is the current board is winning
-            print('I won!!')
-            goalReached=True
+            start,goalReached, v = cur_state(node,visited,goalStateB,start)
+            nodes.append(node)
 
-        if len(nodes) == 0: return None
-        tmp = np.array(node.board)
+        elif len(node.moves)!=0 and node.depth<depth_limit:
 
-        br, bc = getBlankPosition(tmp)  # getting current position
-        node.moves=cur_state(tmp)
-       # temp = setMove(node.moves[0], br, bc, temp)  # making my move
-        moves_size=len(node.moves)
+            start, goalStateB, v = cur_state(node, visited, goalStateB,start)
+            if node.has_children():
+                child = node.children.pop(0)
+                nodes.append(child)
+           # elif v:
+             #   m = list(node.moves)
+               # m.pop(0)
+        elif len(node.moves)== 0 and node.depth < depth_limit:
 
-
-        for i in range(moves_size):
-            temp = np.array(node.board)
-            temp_move = setMove(node.moves[0], br, bc, temp)
-            node.moves.pop(0)
-            state = temp_move.ravel()  # make temp flat
-            state = int(''.join(map(str, state)))
-            if state not in visited:
-                if np.all(state==goal):
-                    print "win win"
-                    break
-                else:
-                    visited.add(state)
-                    print "state is in the bucket", state
-                    #node.visited = True
-                    new_moves = cur_state(temp_move)
-                    d = node.depth + 1
-                    children = node.add_child(create_node(temp_move, node, None, new_moves, d, False))
+                nodes.pop(0)
+                node=nodes[0]
+        elif len(node.moves)==0 and node.parent!=None:
+            node=node.parent
+        elif node.depth == depth_limit:
+            if len(nodes) > 1:
+                nodes.pop(0)
+                node=nodes[0]
             else:
-                print "This state was visited already, skipping to the next move"
-
-        node.visited = True
+                print "I explored the tree to the given maximum depth"
+                break
 
 
 def dfs(node, goal, depth):
@@ -219,86 +217,32 @@ def dfs(node, goal, depth):
     OFM=False       #Out Of Moves Boolean
     start=True
     d=0
+    goalStateB=False
 
     while goalReached==False:
 
+
         if len(nodes)==0 and start==True:       #Appending the root node
-            start=False
+
+            start,goalReached, v = cur_state(node,visited,goalStateB,start)
             nodes.append(node)
-        elif node.has_children() and node.visited==False:       #If the currently focused node has children, then append the first child . The node focuses on the parent which is in position node_lendth-1
-            node.visited=True
-            node=node.get_children()
-            nodes.append(node)
-        elif node.has_children() and node.parent==None:       #If the currently focused node has children, then append the first child . The node focuses on the parent which is in position node_lendth-1
-            node.visited=True
-            node=node.get_children()
-            nodes.append(node)
-        elif node.visited and OFM:  # If the state of the node has been visited and there are no available moves, remove the node to iterate backwards into the tree
-            node = nodes.pop()
-            node = node.get_parent()
-            node.visited=False
-            OFM=False
-        elif node.visited and OFM==False:                                  #If the state was visited continue with the same node and the rest of it's available moves
-           # node=node.get_parent()
-            node.visited=False
-            print('Still have moves')
-        elif len(nodes)==0:
-            print('I am out of moves')
+
+        elif len(node.moves)!=0 and node.depth<depth_limit:
+            start, goalStateB, v = cur_state(node, visited, goalStateB,start)
+            if node.has_children():
+                node = node.children.pop(0)
+                nodes.append(node)
+           # elif v:
+             #   m = list(node.moves)
+               # m.pop(0)
+
+        elif len(node.moves)==0 and node.parent!=None:
+            node=node.parent
+        elif node.depth == depth_limit:
+            node=node.parent
+        elif len(node.moves)==0 and node.parent==None :
+            print "explored the tree to its depth limit and i am out of moves"
             break
-
-
-
-        print('my board', node.board)
-
-        if np.all(node.board == goal):                  #Checking is the current board is winning
-            print('I won!!')
-            goalReached=True
-
-        if len(nodes) == 0: return None
-        temp = np.array(node.board)
-
-        if node.depth <= depth_limit:
-
-            if len(node.moves)!= 0:
-                temp1=temp
-                br,bc=getBlankPosition(temp1)    #getting current position
-                temp1=setMove(node.moves[0],br,bc,temp)   #making my move
-                node.moves.pop(0)
-                state = temp1.ravel()     #make temp flat
-                state = int(''.join(map(str, state)))
-            if state not in visited:
-
-                if np.all(state == goal):                  #Checking is the current board is winning
-                    print('I won!!')
-                    goalReached=True
-                    break
-                else:
-                    visited.add(state)
-                    #node.visited = True
-                    new_moves = cur_state(temp1)
-                    d = node.depth +1
-                    children=node.add_child(create_node(temp1, node, None, new_moves, d,False))
-
-            elif len(node.moves)!=0:
-                OFM=False
-                node.visited = True
-                continue
-            elif len(node.moves)==0:
-                OFM=True
-                node.visited=True
-             #   node = nodes.pop()
-                continue
-        elif node.depth==depth_limit:                       #TB fixed
-            print('Checking the depth', node.depth)
-            node=nodes.pop()
-            node=node.get_parent()
-         #   node.visited=True
-
-            continue
-        elif node.depth>depth_limit:
-            print('I am out of moves. My goal is' , goal ,' These are the states visited ', visited)
-
-
 
 
 
@@ -352,19 +296,21 @@ def main():
     #board =np.array([[2, 8, 3], [1, 6, 4], [7, 0, 5]])
     board = np.array([[1, 2, 3, ], [4, 0, 5], [6, 7, 8]])
     #board = np.array([[2, 8,3], [1, 6, 4], [7, 0, 5]])
+    #board = np.array([[3, 1,2], [0, 6, 4], [7, 8, 5]])
 
     moves = legal_moves(board)
+
 
     root = Node(board, None, None, moves, 0,False)
 
     states = createStateList(board)
 
     s = setStates(states,board)
+    print s
+    #dfs(root, s, 20)
 
-    #dfs(root, s, 3)
 
-
-    bfs(root, s, 1)
+    bfs(root, s, 3)
 
 if __name__ == '__main__':
     main()
